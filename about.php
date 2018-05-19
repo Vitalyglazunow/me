9706509c3dd9a0773e7dd6680a419e9414570c6b
Vitalyglazunow <39440595+Vitalyglazunow@users.noreply.github.com>
2018-05-19 20:38:32 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php
+$name = 'Виталий';
+$age = 34;
+$email = 'vitalyglazunow@gmail.com';
+$city = 'Москва';
+$about = 'Хотел бы больше узнать информации по программированию на php';
+?>
+<!DOCTYPE>
+<html lang="ru">
+    <head>
+        <title><?= $name . ' – ' . $about ?></title>
+        <meta charset="utf-8">
+        <style>
+            body {
+                font-family: sans-serif;  
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Страница пользователя <?= $name ?></h1>
+        <dl>
+            <dt>Имя</dt>
+            <dd><?= $name ?></dd>
+            <dt>Возраст</dt>
+            <dd><?= $age ?></dd>
+            <dt>Адрес электронной почты</dt>
+            <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
+            <dt>Город</dt>
+            <dd><?= $city ?></dd>
+            <dt>О себе</dt>
+            <dd><?= $about ?></dd>
+        </dl>
+    </body>
+</html>
