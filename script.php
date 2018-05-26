5a72cd5f92131a96eb157cbcbf3097efa6a87e39
Vitalyglazunow <39440595+Vitalyglazunow@users.noreply.github.com>
2018-05-26 13:10:37 +0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,39 @@
+﻿<html lang="ru">
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+<form method="GET">
+    Число: <input type="text" name="x">
+    <input type="submit" value="Отправить">
+</form>
+
+
+<?php
+echo "<br>Число = ". $_GET['x'];
+$x = $_GET['x'];
+$odin = 1;
+$dva = 1;
+echo "<br>";
+
+while(true) {
+    if ($odin > $x) {
+        echo "Задуманное число НЕ входит в числовой ряд";
+        break;
+    }
+    elseif ($odin == $x) {
+        echo "Задуманное число входит в числовой ряд";
+        break;
+    }
+    else {
+        $tri = $odin;
+        $odin = $odin + $dva;
+        $dva = $tri;
+        echo $odin; echo "<br>";
+    }
+}
+?>
+
+</body>
+</html>
